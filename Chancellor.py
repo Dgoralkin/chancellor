a14f5dfcd0c420b4991c2fb9b3877ecc877f8d81
Dany Goralkin <97537164+Dgoralkin@users.noreply.github.com>
2023-11-10 01:32:37 -0800

    Update Chancellor.py
    Final version

--- Chancellor.py
+++ Chancellor.py
@@ -1,11 +1,13 @@
 # Designed and built by Daniel Gorelkin IT-140-X2058 23EW2 (11/11/2023)
 
+
 # Define two options for the menu prompt to be chosen and called by user.
 menu_prompt = ('To move: \t\t"N"-north, "S"-south, "E"-east, "W"-west \n'
                'To pick item: \t"Get item name"\n'
                'Instructions: \t"Help"\n'
                'To quit: \t\t"Quit"\n\n')
 short_menu_prompt = 'Need help? Just type "Help". Or, "Expand", to get navigation directions.\n'
+
 
 # Define room specs in a dict. Set by room name, number, item, and adjacent rooms.
 rooms = {
@@ -31,6 +33,7 @@ rooms = {
                   'History': 'None'}
 }
 
+
 # Print the initial main menu and the game commands instructions.
 def instructions():
     print("\nWellcome to the Chancellor Text Adventure Quest!\n\n"
@@ -45,6 +48,7 @@ def instructions():
     print(23 * "=", end='\n\n')  # Decoration
     main()  # Sends the controls to the commands block.
 
+
 # Define the current room location print block.
 def print_room():
     # Set modular active decoration.
@@ -54,15 +58,14 @@ def print_room():
     # Pulls data from global room dict.
     print(f'You are in the', rooms[rooms['Temp_room']['name']]['name'], f'room.')
 
-    # ------------------------------------------------------------------------------------------
+    # Pulls room name from dict and stores it in var 'room'
     room = rooms['Temp_room']['name']
-    room_item = rooms[room]['item']
-    if room_item != 'None':
-        print('You see a {}.'.format(room_item))
-        print('To get it, enter "Get {}".'.format(room_item))
-    # ------------------------------------------------------------------------------------------
+    room_item = rooms[room]['item']  # Pulls room item from dict
+    if room_item != 'None':  # Validates room item is in the room
+        print('You see a {}.'.format(room_item))  # Update prompt
+        print('To get it, enter "Get {}".'.format(room_item))  # Instruction prompt
 
-    # Present inventory on hand and to collect.
+    # Present inventory on hand and to be collected.
     inventory_items = []  # Set list to store collected items
     counter = 0  # Set counter to count collected items
     # Iterate through the items collected from inventory dict
@@ -72,12 +75,12 @@ def print_room():
         if item == 'None':  # Skip uncollected items
             continue
         else:
-            len_counter += len(item) + 4
+            len_counter += len(item) + 4  # update counter
             inventory_items.append(item)  # Add collected items to the list and update counter
             counter += 1  # Present items to be collected
             # If counter reaches '6', means 6 items were collected and the game is over.
             if counter == 6:
-                # Print winning statement and quit.
+                # Print winning prompt and quit.
                 print('\nCongratulation! You Won and managed to escape from SNHU!\n'
                       'Thanks for playing the game. Hope you enjoyed it.')
                 exit()
@@ -85,32 +88,46 @@ def print_room():
     print('Your inventory:{} {}/6 collected.'.format(inventory_items, counter))  # Dynamic printout statement.
     print(4 * "=", 9 * "=", len_counter * "=", 14 * "=", end='\n\n')  # Dynamic decoration printout.
 
+
 # Define the room travel block. Keeps track of room location.
 def update_room(c_room, move_room):  # Receives Current room, and direction command.
     if rooms[c_room][move_room] != 'None':  # Validates adjacent room location.
         # If requested room is available, update room location in the global room dict.
         rooms['Temp_room']['name'] = rooms[c_room][move_room]
+
+        # Set counter to count inventory in the villain room
+        inventory_counter = 0
+        if rooms['Temp_room']['name'] == 'Chancellor':  # Catch if user enter the villain room
+            count = rooms['Inventory'].values()  # Pull items from inventory dict
+            for item in count:  # Iterate through the items
+                if item != 'None':  # Count how many items were picked up
+                    inventory_counter += 1  # Update counter
+            if inventory_counter < 6:  # Validate all items were collected
+                # If user enter villain room without all the items, print prompt and exit program
+                input('\nOh, NO! The Chancellor spotted you without you having all the item handy!'
+                      '\nThanks for playing the game. It\'s over. Hope you enjoyed it.')
+                exit()
     else:
         # Print prompt and send back to the command block.
-        print("I'm sorry. You can’t go that way!")
+        input("I'm sorry. You can’t go that way!")
         main()
     main()
 
-# define inventory management function 'inventory'
+
+# define inventory management function 'inventory' to manage collected item
 def inventory(room, command):
-    tmp_item = rooms[room]['item']
-    item = command.split()
+    tmp_item = rooms[room]['item']  # Pulls item from dict and stores it in var 'tmp_item'
+    item = command.split()  # Splits the input command 'get item_name' and stores it in var
+    # Validate 'get item_name was correctly entered by user.'
     if len(item) == 2 and item[0] == 'get' and item[-1] == tmp_item:
-        rooms[room]['item'] = 'None'
-        rooms['Inventory'][room] = item[-1]
-        input('\nYou got the {}!\n'.format(item[-1]))
+        rooms[room]['item'] = 'None'  # Removes the item from its room.
+        rooms['Inventory'][room] = item[-1]  # Transfers the item into the inventory dict.
+        input('\nYou got the {}!\n'.format(item[-1]))  # Updates user about the picked up item
         return
     else:
-        input('Please type in "Get {}" to pick up the item.'.format(tmp_item))
-        main()
+        input('Please type in "Get {}" to pick up the item.'.format(tmp_item))  # Update message with guidance.
+        main()  # Call/return to main function
 
-
-# ------------------------------------------------------------------------------------------------------
 
 # Define the game commands block with optional argument reception.
 def main(*prompt):
@@ -131,12 +148,11 @@ def main(*prompt):
     if prompt == 1:  # Checks if user requested expanded control menu
         text = menu_prompt  # Sets the menu
     else:
-        text = short_menu_prompt
-    # Read user command input with embedded prompt and store it.
-    command = input(text).lower().strip()
+        text = short_menu_prompt  # Sets the menu
+    command = input(text).lower().strip()  # Read user command input with embedded prompt and store it.
 
     # Command filter block.
-    # If navigation commands (e/s/n/w) received, CALL update_room block and send the room and navigation values'''
+    # If navigation commands (e/s/n/w) received, CALL update_room function and pass room + navigation values'''
     if command == 'e':
         update_room(rooms['Temp_room']['name'], 'east')
     elif command == 's':
@@ -154,11 +170,12 @@ def main(*prompt):
     elif command == 'expand':
         main(1)  # Send change prompt argument to the command block.
     elif 'get' in command:
-        inventory(rooms['Temp_room']['name'], command)
+        inventory(rooms['Temp_room']['name'], command)  # If command is get item: CALL 'inventory' and send args
         main()
     else:  # Repeat command loop if input is invalid.
         print('\n"' + command + '"' + ' is unrecognized command.')
         main()
+
 
 # Run program by demand.
 if __name__ == '__main__':
